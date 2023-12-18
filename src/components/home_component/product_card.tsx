de0e7349df08812024e699fb22b2f2b504e6b36d
egileekada <egileoniso.ekada@gmail.com>
2023-12-18 18:34:55 +0100

    fixing responsive issuse

--- src/components/home_component/product_card.tsx
+++ src/components/home_component/product_card.tsx
@@ -1,14 +1,14 @@
 // import React from 'react'
- 
-import BuyRepBtn from "../project_component/buy_rep_btn"
-import { useNavigate } from "react-router-dom" 
 
-function ProductCard() { 
+import BuyRepBtn from "../project_component/buy_rep_btn"
+import { useNavigate } from "react-router-dom"
+
+function ProductCard() {
 
     const navigate = useNavigate()
 
     return (
-        <div role="button" onClick={()=> navigate("/project")} className="w-full h-[249px] flex flex-col bg-white rounded-[10px] p-4 lg:p-6 justify-center ">
+        <div role="button" onClick={() => navigate("/project")} className="w-full h-[249px] flex flex-col bg-white rounded-[10px] p-4 lg:p-6 justify-center ">
             <div className=" flex items-center gap-3 " >
                 <div className=" w-fit " >
                     <div className="w-[80px] h-[80px] lg:w-[120px] lg:h-[120px] relative">
@@ -24,22 +24,23 @@ function ProductCard() {
                     <div className="text-[#00990F] text-lg font-bold ">Price: $5,868</div>
                 </div>
             </div>
-            <div className=" w-full flex gap-1 lg:gap-8 items-center mt-4 lg:mt-6 " >
-
-                <div className=" border-r pr-1 lg:pr-8 ">
-                    <p className="text-slate-900 lg:text-2xl font-bold  leading-loose">$121,345</p>
-                    <p className="text-zinc-500 lg:text-lg text-xs font-normal ">mkt cap</p>
-                </div>
-                <div className="border-r pr-1 lg:pr-8 ">
-                    <p className="text-slate-900 lg:text-2xl font-bold  leading-loose">$53,235</p>
-                    <p className="text-zinc-500 lg:text-lg text-xs font-normal ">usd locked</p>
-                </div>
-                <div className="">
-                    <p className="text-slate-900 lg:text-2xl font-bold  leading-loose">10%</p>
-                    <p className="text-zinc-500 lg:text-lg text-xs font-normal ">CR</p>
+            <div className=" w-full flex lg:flex-row flex-col gap-3 lg:gap-8 items-start mt-4 lg:mt-6 " >
+                <div className=" flex gap-2 " > 
+                    <div className=" border-r pr-2 lg:pr-8 ">
+                        <p className="text-slate-900 lg:text-2xl font-bold  leading-loose">$121,345</p>
+                        <p className="text-zinc-500 lg:text-lg text-xs font-normal ">mkt cap</p>
+                    </div>
+                    <div className="border-r pr-2 lg:pr-8 ">
+                        <p className="text-slate-900 lg:text-2xl font-bold  leading-loose">$53,235</p>
+                        <p className="text-zinc-500 lg:text-lg text-xs font-normal ">usd locked</p>
+                    </div>
+                    <div className="">
+                        <p className="text-slate-900 lg:text-2xl font-bold  leading-loose">10%</p>
+                        <p className="text-zinc-500 lg:text-lg text-xs font-normal ">PR</p>
+                    </div>
                 </div>
                 <BuyRepBtn product={true} />
-            </div> 
+            </div>
         </div>
     )
 }
